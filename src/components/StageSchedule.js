5288a7c0298a0269fb5d85494a648743f6aa0913
Goodricke <tech@goodricke.co.uk>
2025-04-25 20:16:22 +0100

    Update Band Socials

--- src/components/StageSchedule.js
+++ src/components/StageSchedule.js
@@ -8,32 +8,32 @@ export const StageSchedule = {
   Schedule: [
     {
       Act: "The Tonics",
-      Socials: [""],
+      Socials: ["https://www.instagram.com/thetonicsband/"],
       Time: "17:30-17:55",
     },
     {
       Act: "Cavalier",
-      Socials: ["https://www.instagram.com/cavalier.band/"],
+      Socials: ["https://www.instagram.com/cavalier.band/","https://www.youtube.com/@Band.Cavalier"],
       Time: "18:05-18:60",
     },
     {
       Act: "Substate",
-      Socials: [""],
+      Socials: ["https://www.instagram.com/substate.band/","https://www.tiktok.com/@substate.band"],
       Time: "18:40-19:05",
     },
     {
       Act: "Catch",
-      Socials: [""],
+      Socials: ["https://www.instagram.com/catchband_/","linktr.ee/catchband"],
       Time: "19:15-19:40",
     },
     {
       Act: "The Expression",
-      Socials: [""],
+      Socials: ["https://www.instagram.com/the.expression.band/"],
       Time: "19:50-20:15",
     },
     {
       Act: "The Blazers",
-      Socials: [""],
+      Socials: ["https://www.instagram.com/theblaz3rs/","https://theblazersband.com/"],
       Time: "20:25-20:50",
     },
   ],
